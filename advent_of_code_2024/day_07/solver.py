b4b09b52403c8278b74edd9fa8c6ea747f36d663
Keith Dao <keith.dao.248@gmail.com>
2024-12-08 03:57:43 +1000

    Add day 7 solver.

--- advent_of_code_2024/day_07/solver.py
+++ advent_of_code_2024/day_07/solver.py
@@ -2,7 +2,9 @@
 Day 7 solver.
 """
 
+import math
 from pathlib import Path
+import sys
 
 from ..base import Solver as BaseSolver
 
@@ -12,8 +14,66 @@ class Solver(BaseSolver):
     Day 7 solver.
     """
 
+    def _check(self, nums, target, check_concat=False) -> bool:
+        """
+        Checks if a sequence of numbers to can result in target.
+
+        Args:
+            num: The sequence of nums in order.
+            target: The target value to reach.
+            check_concat: Check if the concatenation operator can be used.
+
+        Returns:
+            True if the sequence can reach target, else false.
+        """
+        queue = {target}
+
+        for x in reversed(nums):
+            new_queue = set()
+            for total in queue:
+                if total == 0:
+                    continue
+
+                if total - x >= 0:
+                    new_queue.add(total - x)
+
+                if total % x == 0:
+                    new_queue.add(total // x)
+
+                if (
+                    check_concat
+                    and total % (tens := 10 ** (int(math.log10(x)) + 1)) == x
+                ):
+                    new_queue.add(total // tens)
+
+            queue = new_queue
+
+        return 0 in queue
+
+    def _solve(self, filepath: Path, is_part_two: bool = False) -> int:
+        """
+        Solves the part.
+
+        Args:
+            filepath: The path to the file input.
+            is_part_two: True if solving for part two, else false.
+
+        Returns:
+            The solution to the part.
+        """
+        result = 0
+        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
+            for line in file:
+                total, nums = line.strip().split(":")
+                total = int(total)
+                nums = [int(x) for x in nums.strip().split()]
+                if self._check(nums, total, is_part_two):
+                    result += total
+
+        return result
+
     def part_1(self, filepath: Path) -> int:
-        return 1
+        return self._solve(filepath, False)
 
     def part_2(self, filepath: Path) -> int:
-        return 1
+        return self._solve(filepath, True)
--- test/test_day_07.py
+++ test/test_day_07.py
@@ -11,6 +11,14 @@ class TestDay7(BaseTests):
     """Tests the day 7 solver."""
 
     solver = Solver()
-    example_input = """###"""
-    part_1_solution = -1
-    part_2_solution = -1
+    example_input = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+    part_1_solution = 3749
+    part_2_solution = 11387

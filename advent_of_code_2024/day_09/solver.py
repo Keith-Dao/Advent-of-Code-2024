9f5db21711a293fa072d5642b1dd3bb5944b0053
Keith Dao <keith.dao.248@gmail.com>
2024-12-09 22:07:53 +1000

    Improve summation.

--- advent_of_code_2024/day_09/solver.py
+++ advent_of_code_2024/day_09/solver.py
@@ -52,14 +52,14 @@ class Solver(BaseSolver):
             line = file.read().strip()
 
         files = []
-        free_spaces_per_length = [[] for _ in range(10)]
+        free_spaces_with_length = [[] for _ in range(10)]
         current = 0
         for i, c in enumerate(line):
             x = int(c)
             if i % 2 == 0:
                 files.append([current, x])
             else:
-                free_spaces_per_length[x].append(current)
+                free_spaces_with_length[x].append(current)
 
             current += x
 
@@ -68,32 +68,31 @@ class Solver(BaseSolver):
 
             selected_len = -1
             for length in range(file_len, 10):
-                if not free_spaces_per_length[length]:
+                if not free_spaces_with_length[length]:
                     continue
 
                 if (
                     selected_len == -1
-                    or free_spaces_per_length[length][0]
-                    < free_spaces_per_length[selected_len][0]
+                    or free_spaces_with_length[length][0]
+                    < free_spaces_with_length[selected_len][0]
                 ):
                     selected_len = length
 
             if (
                 selected_len == -1
-                or file_start < free_spaces_per_length[selected_len][0]
+                or file_start < free_spaces_with_length[selected_len][0]
             ):
                 continue
 
-            new_start = heapq.heappop(free_spaces_per_length[selected_len])
+            new_start = heapq.heappop(free_spaces_with_length[selected_len])
             file[0] = new_start
             if selected_len > file_len:
                 heapq.heappush(
-                    free_spaces_per_length[selected_len - file_len],
+                    free_spaces_with_length[selected_len - file_len],
                     new_start + file_len,
                 )
 
         return sum(
-            i * x
-            for x, (start, l) in enumerate(files)
-            for i in range(start, start + l)
+            x * (start * length + length * (length - 1) // 2)
+            for x, (start, length) in enumerate(files)
         )

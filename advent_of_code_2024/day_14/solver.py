728917fa3db83600ff9081d4ec6ce2a6c188b91a
Keith Dao <keith.dao.248@gmail.com>
2024-12-14 18:58:41 +1000

    Add day 14 solver.

--- advent_of_code_2024/day_14/solver.py
+++ advent_of_code_2024/day_14/solver.py
@@ -3,6 +3,7 @@ Day 14 solver.
 """
 
 from pathlib import Path
+import sys
 
 from ..base import Solver as BaseSolver
 
@@ -12,8 +13,53 @@ class Solver(BaseSolver):
     Day 14 solver.
     """
 
-    def part_1(self, filepath: Path) -> int:
-        return 1
+    def part_1(
+        self, filepath: Path, size: tuple[int, int] = (103, 101)
+    ) -> int:
+        m, n = size
+        locations = []
+        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
+            for line in file:
+                p, v = line.strip().split()
+                p_j, p_i = (int(x) for x in p[2:].split(","))
+                v_j, v_i = (int(x) for x in v[2:].split(","))
+                i = (p_i + v_i * 100) % m
+                j = (p_j + v_j * 100) % n
+                locations.append((i, j))
 
-    def part_2(self, filepath: Path) -> int:
-        return 1
+        quadrant = [0] * 4
+        for i, j in locations:
+            if i == m // 2 or j == n // 2:
+                continue
+            q_i, q_j = i // ((m + 1) // 2), j // ((n + 1) // 2)
+            quadrant[q_i * 2 + q_j] += 1
+
+        result = 1
+        for x in quadrant:
+            result *= x
+        return result
+
+    def part_2(
+        self, filepath: Path, size: tuple[int, int] = (103, 101)
+    ) -> int:
+        m, n = size
+        robots = []
+        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
+            for line in file:
+                p, v = line.strip().split()
+                p_j, p_i = (int(x) for x in p[2:].split(","))
+                v_j, v_i = (int(x) for x in v[2:].split(","))
+                robots.append((p_i, p_j, v_i, v_j))
+
+        result = 0
+        while True:
+            result += 1
+            seen = set()
+            for k, (i, j, d_i, d_j) in enumerate(robots):
+                robots[k] = ((i + d_i) % m, (j + d_j) % n, d_i, d_j)
+                seen.add((robots[k][:2]))
+
+            if len(seen) == len(robots):
+                break
+
+        return result
--- test/test_day_14.py
+++ test/test_day_14.py
@@ -11,4 +11,23 @@ class TestDay14(BaseTests):
     """Tests the day 14 solver."""
 
     solver = Solver()
-    cases = [("""###""", -1, -1)]
+    cases = [
+        (
+            """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3""",
+            12,
+            None,
+        )
+    ]
+
+    test_args = {"size": (7, 11)}
